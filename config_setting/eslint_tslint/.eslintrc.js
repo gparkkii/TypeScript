4649b26514a86615d9c29bb0b41cdda3dd6c72f9
gparkkii <jyp933@gmail.com>
2021-02-05 16:40:11 +0900

    TypeScrypt Tslint Setting

---
+++ config_setting/eslint_tslint/.eslintrc.js
@@ -0,0 +1,39 @@
+// .eslintrc.js
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        trailingComma: 'all',
+        endOfLine: 'lf',
+      },
+    ],
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ['node_modules/'],
+};
