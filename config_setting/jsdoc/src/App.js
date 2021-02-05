2ed910b1765d9a860d09f22c68063699835542bf
gparkkii <jyp933@gmail.com>
2021-02-05 16:40:29 +0900

    TypeScript JSDOC Setting

---
+++ config_setting/jsdoc/src/App.js
@@ -0,0 +1,16 @@
+/**
+ * App 클래스 입니다.
+ * @class
+ */
+const App = function () {
+  /**
+   * message를 입력받아 출력하는 함수입니다.
+   * @param message {string} 출력할 메세지 입니다.
+   */
+  const printMessage = function (message) {
+    console.log(message);
+  };
+  printMessage();
+};
+
+App();
